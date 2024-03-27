7e1daf4cee6602e280a814fce3c407780b0f8372
IOS2004 <lostlevy@gmail.com>
2024-03-27 20:25:05 +0530

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Implementing classes of rooms and section
+class Appliances;
+
+class Room
+{
+	vector<Appliances> appliances;
+	string name;
+	unsigned int quantity;
+public:
+	Room(vector<Appliances> a, string n, unsigned int q)
+	{
+		appliances = a;
+		name = n;
+		quantity = q;
+	}
+	// member functions here
+};
+
+class Sections
+{
+	vector<Room> rooms;
+	vector<Appliances> appliances;
+	string name;
+public:
+	Sections(vector<Room> r, vector<Appliances> a)
+	{
+		rooms = r;
+		appliances = a;
+	}
+	// member functions here
+};
+
+int main()
+{
+
+}
