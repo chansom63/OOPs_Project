fdcc6de034127de165f78304cee64834a64d9444
chansom63 <165356726+chansom63@users.noreply.github.com>
2024-03-29 11:24:53 +0530

    Add files via upload

---
+++ Appliancesclass.cpp
@@ -0,0 +1,20 @@
+
+
+class Appliances
+{
+	string name;
+	double power;
+	unsigned int qty;
+	bool status;
+	bool conditions;
+public:
+    Appliances(string N, double P, unsigned int Q, bool S, bool C)
+	{
+		name=N;
+		Power=p;
+		qty=Q;
+		status=S;
+		condition=C;
+	}
+    // member functions here
+};
