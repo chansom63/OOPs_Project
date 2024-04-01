365d8fd35e5fcac04ff4b7847382929a923b5890
IOS2004 <lostlevy@gmail.com>
2024-04-01 20:22:16 +0530

    added complaints functionality

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#iclude <string>
+#include <string>
 #include <vector>
 using namespace std;
 
@@ -14,7 +14,7 @@ public:
 	Appliances(string N, double P, unsigned int Q, bool S, bool C)
 	{
 		name = N;
-		power = p;
+		power = P;
 		qty = Q;
 		status = S;
 		conditions = C;
@@ -56,8 +56,8 @@ class PowerSource
 	unsigned int qty;
 	double PowerRating;
 public:
-	PowerSource (){}
-	PowerSource (unsigned int q, double rate)
+	PowerSource() {}
+	PowerSource(unsigned int q, double rate)
 	{
 		qty = q;
 		PowerRating = rate;
@@ -72,12 +72,12 @@ class Admin
 	PowerSource RegularSupply;
 	vector<Room> Rooms;
 public:
-	Admin (vector<Sections> sections,
-	PowerSource solar,
-	PowerSource regularSupply,
-	vector<Room> rooms)
+	Admin(vector<Sections> sections,
+		PowerSource solar,
+		PowerSource regularSupply,
+		vector<Room> rooms)
 	{
-		Sections = sections;
+		sections = sections;
 		Solar = solar;
 		RegularSupply = regularSupply;
 		Rooms = rooms;
@@ -87,44 +87,44 @@ public:
 
 class ComplaintRecord;
 
-class Complaints 
+class Complaints
 {
 	string complaintID;
 	string complainant;
-	char PhNo[11];
+	string PhNo;
 	string Description;
 	string FaultLocation;
-	char Date[9];
+	string Date;
 	bool status;
 	friend class ComplaintRecord;
 public:
-	Complaints (string complaintID,
-	string complainant,
-	char PhNo[11],
-	string Description,
-	char Date[8],
-	bool status,
-	string FaultLocation)
+	Complaints(string complaintID,
+		string complainant,
+		string PhNo,
+		string Description,
+		string Date,
+		bool status,
+		string FaultLocation)
 	{
-		this->complaintID=complaintID;
-		this->complainant=complainant;
-		strcpy(this->PhNo,PhNo);
-		this->Description=Description;
-		strcpy(this->Date,Date);
-		this->status=status;
-		this->FaultLocation=FaultLocation;
+		this->complaintID = complaintID;
+		this->complainant = complainant;
+		PhNo = PhNo;
+		this->Description = Description;
+		Date = Date;
+		this->status = status;
+		this->FaultLocation = FaultLocation;
 	}
 };
 
 class ComplaintRecord {
-    vector<Complaints> List;
+	vector<Complaints> List;
 public:
 	ComplaintRecord()
 	{
-		Complaints c1("CN001","Somesh Chandra","6745910345","Projector not Working","25-03-24",0,"LT-II 203");
-		Complaints c2("CN002","Amit","7459160345","Sound System not Working","28-03-24",1,"LT-I 106");
-		Complaints c3("CN003","Aman","4591034985","Fan not Working","31-03-24",0,"CSE LAB-C123A");
-		Complaints c4("CN004","Sam","5910376945","System-21 not Working","05-04-24",0,"CEF GENERIC");
+		Complaints c1("CN001", "Somesh Chandra", "6745910345", "Projector not Working", "25-03-24", 0, "LT-II 203");
+		Complaints c2("CN002", "Amit", "7459160345", "Sound System not Working", "28-03-24", 1, "LT-I 106");
+		Complaints c3("CN003", "Aman", "4591034985", "Fan not Working", "31-03-24", 0, "CSE LAB-C123A");
+		Complaints c4("CN004", "Sam", "5910376945", "System-21 not Working", "05-04-24", 0, "CEF GENERIC");
 		List.push_back(c1);
 		List.push_back(c2);
 		List.push_back(c3);
@@ -133,83 +133,83 @@ public:
 
 	void display()
 	{
-		char selector;int check=0;
-		cout<<"\n\n---------------------------------Complaint Portal----------------------------------------------";
-		if(List.size()==0){cout<<"\nNo complaints exist"; return;}
-		cout<<"\nEnter the following keys to view Complaints:-"<<endl
-		<<"1.View Pending complaints."<<endl
-		<<"2.View Resolved complaints."<<endl
-		<<"3.View all Complaints"<<endl
-		<<"Press the key:"<<endl;
-		cin>>selector;
-		
+		char selector; int check = 0;
+		cout << "\n\n---------------------------------Complaint Portal----------------------------------------------";
+		if (List.size() == 0) { cout << "\nNo complaints exist"; return; }
+		cout << "\nEnter the following keys to view Complaints:-" << endl
+			<< "1.View Pending complaints." << endl
+			<< "2.View Resolved complaints." << endl
+			<< "3.View all Complaints" << endl
+			<< "Press the key:" << endl;
+		cin >> selector;
+
 		//Checker block
-		while(!check)
+		while (!check)
 		{
-		   switch(selector)
-		   {
-		   case '1':
-		   case '2':
-		   case '3':
-		    check=1;break;
-		   default:
-		    fflush(stdin);
-		    cout<<"\nInvalid input"<<endl
-		    <<"Press the key again:";
-		    cin>>selector;
-		   }
-		}
-		switch(selector)
-		{
+			switch (selector)
+			{
 			case '1':
-			for(int i=0;i<List.size();i++)
-			{ 
-				if(List[i].status!=0) continue;
-				cout<<"\nComplaint ID:"<<List[i].complaintID;
-				cout<<"\nComplainant:"<<List[i].complainant;
-				cout<<"\nPhoneNo.:"<<List[i].PhNo;
-				cout<<"\nDescription:"<<List[i].Description;
-				cout<<"\nDate Of Complaint:"<<List[i].Date;
-				cout<<"\nFault Location:"<<List[i].FaultLocation;
-				cout<<"\nstatus:";
-				(List[i].status)?cout<<"Resolved":cout<<"Pending";
-				cout<<endl<<endl;
-			}
-			break;
 			case '2':
-			for(int i=0;i<List.size();i++)
-			{
-				if(List[i].status!=1) continue;
-				cout<<"\nComplaint ID:"<<List[i].complaintID;
-				cout<<"\nComplainant:"<<List[i].complainant;
-				cout<<"\nPhoneNo.:"<<List[i].PhNo;
-				cout<<"\nDescription:"<<List[i].Description;
-				cout<<"\nDate Of Complaint:"<<List[i].Date;
-				cout<<"\nFault Location:"<<List[i].FaultLocation;
-				cout<<"\nstatus:";
-				(List[i].status)?cout<<"Resolved":cout<<"Pending";
-				cout<<endl<<endl;
-			}
-			break;
-	
 			case '3':
-			for(int i=0;i<List.size();i++)
+				check = 1; break;
+			default:
+				fflush(stdin);
+				cout << "\nInvalid input" << endl
+					<< "Press the key again:";
+				cin >> selector;
+			}
+		}
+		switch (selector)
+		{
+		case '1':
+			for (int i = 0; i < List.size(); i++)
 			{
-				cout<<"\nComplaint ID:"<<List[i].complaintID;
-				cout<<"\nComplainant:"<<List[i].complainant;
-				cout<<"\nPhoneNo.:"<<List[i].PhNo;
-				cout<<"\nDescription:"<<List[i].Description;
-				cout<<"\nDate Of Complaint:"<<List[i].Date;
-				cout<<"\nFault Location:"<<List[i].FaultLocation;
-				cout<<"\nstatus:";
-				(List[i].status)?cout<<"Resolved":cout<<"Pending";
-				cout<<endl<<endl;
+				if (List[i].status != 0) continue;
+				cout << "\nComplaint ID:" << List[i].complaintID;
+				cout << "\nComplainant:" << List[i].complainant;
+				cout << "\nPhoneNo.:" << List[i].PhNo;
+				cout << "\nDescription:" << List[i].Description;
+				cout << "\nDate Of Complaint:" << List[i].Date;
+				cout << "\nFault Location:" << List[i].FaultLocation;
+				cout << "\nstatus:";
+				(List[i].status) ? cout << "Resolved" : cout << "Pending";
+				cout << endl << endl;
 			}
 			break;
-			default:
-			cout<<"\nInvalid Input";
+		case '2':
+			for (int i = 0; i < List.size(); i++)
+			{
+				if (List[i].status != 1) continue;
+				cout << "\nComplaint ID:" << List[i].complaintID;
+				cout << "\nComplainant:" << List[i].complainant;
+				cout << "\nPhoneNo.:" << List[i].PhNo;
+				cout << "\nDescription:" << List[i].Description;
+				cout << "\nDate Of Complaint:" << List[i].Date;
+				cout << "\nFault Location:" << List[i].FaultLocation;
+				cout << "\nstatus:";
+				(List[i].status) ? cout << "Resolved" : cout << "Pending";
+				cout << endl << endl;
+			}
+			break;
+
+		case '3':
+			for (int i = 0; i < List.size(); i++)
+			{
+				cout << "\nComplaint ID:" << List[i].complaintID;
+				cout << "\nComplainant:" << List[i].complainant;
+				cout << "\nPhoneNo.:" << List[i].PhNo;
+				cout << "\nDescription:" << List[i].Description;
+				cout << "\nDate Of Complaint:" << List[i].Date;
+				cout << "\nFault Location:" << List[i].FaultLocation;
+				cout << "\nstatus:";
+				(List[i].status) ? cout << "Resolved" : cout << "Pending";
+				cout << endl << endl;
+			}
+			break;
+		default:
+			cout << "\nInvalid Input";
 		}
-		cout<<"\n\n-----------------------------------------------------------------------------------------------";
+		cout << "\n\n-----------------------------------------------------------------------------------------------";
 	}
 };
 
